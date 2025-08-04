0ff29896e09b9f3b32e33ab6fe8041fb7f923af1
Solomon-Asefa <soloasefa6603@gmail.com>
2025-08-04 11:43:58 +0300

    Initial module structure for Customer Sales Analysis

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Customer Sales Analysis',
+    'version': '1.0',
+    'category': 'Sales',
+    'depends': ['base', 'sale'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
